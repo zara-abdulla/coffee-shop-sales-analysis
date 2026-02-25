d22c8175089b17909292e7e181db749164de0f7e
Zahra Abdullayeva <zaraabdulla95@gmail.com>
2026-02-25 11:43:21 +0000

    .

--- .venv/lib/python3.12/site-packages/niquests/extensions/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-This subpackage hold anything that is very relevant
-to the HTTP ecosystem but not per-say Niquests core logic.
-"""
--- .venv/lib/python3.12/site-packages/qh3/_compat.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-
-import sys
-
-DATACLASS_KWARGS = {"slots": True} if sys.version_info >= (3, 10) else {}
-UINT_VAR_MAX = 0x3FFFFFFFFFFFFFFF
-UINT_VAR_MAX_SIZE = 8
